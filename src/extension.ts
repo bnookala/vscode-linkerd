13d99617aa81451c90de85bf2ee755333606c136
Bhargav Nookala <nooknb@gmail.com>
2019-03-27 17:33:34 -0700

    Adding check and install stubs

--- src/extension.ts
+++ src/extension.ts
@@ -18,15 +18,59 @@ export async function activate(context: vscode.ExtensionContext) {
 	}
 
     const subscriptions = [
-        vscode.commands.registerCommand('vscodelinkerd.install', installLinkerd)
+		vscode.commands.registerCommand('vslinkerd.install', installLinkerd),
+		vscode.commands.registerCommand('vslinkerd.check', checkLinkerd)
     ];
 
     context.subscriptions.push(...subscriptions);
 }
 
-function installLinkerd() {
+/**
+ * Installing *should*:
+ * - checks if linkerd is installed, return if it is
+ * - perform a pre-check, which should render in a document provider
+ * - perform an install if the pre-check succeeds, and the user consents
+ * - performs a post-check after the installation.
+ * @param commandTarget
+ */
+function installLinkerd (commandTarget: any) {
+	const clusterName = clusterNode(commandTarget);
+	if (!clusterName) {
+		return undefined;
+	}
+
     console.log("Installing linkerd");
 }
 
+function checkLinkerd (commandTarget: any) {
+	const clusterName = clusterNode(commandTarget);
+	if (!clusterName) {
+		return undefined;
+	}
+
+	console.log("Checking linkerd");
+}
+
+function clusterNode(commandTarget: any): string | undefined {
+    if (!commandTarget) {
+        return undefined;
+	}
+
+    if (!clusterExplorer) {
+        return undefined;
+    }
+
+    const node = clusterExplorer.resolveCommandTarget(commandTarget);
+    if (node) {
+        // We install Linkerd at the cluster level, which appears in the
+        // cluster explorer as a context node.
+        if (node.nodeType === 'context') {
+            return node.name;
+        }
+    }
+
+     return undefined;
+}
+
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate () {}
