ddeb456193749cd60390eb04f082594c83b82d21
Bhargav Nookala <nooknb@gmail.com>
2019-03-30 10:00:45 -0700

    Read binary path from config

--- src/linkerd/linkerd.ts
+++ src/linkerd/linkerd.ts
@@ -1,12 +1,28 @@
 import * as vscode from 'vscode';
 import * as shelljs from 'shelljs';
 import * as k8s from 'vscode-kubernetes-tools-api';
+import * as config from './config';
 
 const outputChannel = vscode.window.createOutputChannel("Linkerd");
 
+const exec = (args: string): shelljs.ExecOutputReturnValue | undefined => {
+    const binaryPath = config.linkerdPath();
+
+    if (!binaryPath) {
+        vscode.window.showErrorMessage("Linkerd binary not found on file system. Please set in workspace settings.");
+        return undefined;
+    }
+
+    return shelljs.exec(`${binaryPath} ${args}`);
+};
+
 export function check () {
     outputChannel.clear();
-    const out = shelljs.exec("linkerd check --pre");
+    const out = exec('check --pre');
+
+    if (!out) {
+        return;
+    }
 
     const checkOutput = out.stdout.toString();
     outputChannel.append(checkOutput);
@@ -27,7 +43,12 @@ export function install (kubectl: k8s.KubectlV1 | undefined) {
         return;
     }
 
-    const out = shelljs.exec("linkerd install");
+    const out = exec("install");
+
+    if (!out) {
+        return;
+    }
+
     const output = out.stdout.toString();
 
     vscode.window.showInformationMessage("Linkerd checks passed. Continuing to install.");
