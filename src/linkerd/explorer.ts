853669cf98cb7807e6cd9c47c5fd54a91892e442
Bhargav Nookala <nooknb@gmail.com>
2019-04-30 14:44:46 -0700

    Node contributor uses namespaces

--- src/linkerd/explorer.ts
+++ src/linkerd/explorer.ts
@@ -4,27 +4,30 @@ import { InstallController } from './install';
 import { exec } from './exec';
 
 
+export interface NamespaceToResource {
+    [key: string]: Array<string>;
+}
+
 export class MeshedResourceExplorer implements k8s.ClusterExplorerV1.NodeContributor {
-    private kubectl: k8s.KubectlV1 | undefined;
-    private installController: InstallController | undefined;
+    constructor (
+        private readonly kubectl: k8s.KubectlV1,
+        private readonly installController: InstallController
+    ) {}
 
-    constructor (kubectl: k8s.KubectlV1, installController: InstallController ) {
-        this.kubectl = kubectl;
-        this.installController = installController;
-    }
-
-    contributesChildren(parent: k8s.ClusterExplorerV1.ClusterExplorerNode | undefined): boolean {
+    contributesChildren (parent: k8s.ClusterExplorerV1.ClusterExplorerNode | undefined): boolean {
         return !!parent && parent.nodeType === 'context';
     }
 
-    async getChildren(_parent: k8s.ClusterExplorerV1.ClusterExplorerNode | undefined): Promise<k8s.ClusterExplorerV1.Node[]> {
+    async getChildren (parent: k8s.ClusterExplorerV1.ClusterExplorerNode | undefined): Promise<k8s.ClusterExplorerV1.Node[]> {
         if (!this.installController) {
+            // Todo: return an error node.
             return [];
         }
 
         const isInstalled = await this.installController.isInstalled();
 
         if (isInstalled.result === false) {
+            // Todo: return an error node.
             return [];
         }
 
@@ -34,7 +37,7 @@ export class MeshedResourceExplorer implements k8s.ClusterExplorerV1.NodeContrib
 
 
 class MeshedResourceFolderNode implements k8s.ClusterExplorerV1.Node {
-    async getChildren(): Promise<k8s.ClusterExplorerV1.Node[]> {
+    async getChildren (): Promise<k8s.ClusterExplorerV1.Node[]> {
         const meshedResources = await exec("get pods --all-namespaces");
 
         if (!meshedResources || meshedResources.code !== 0) {
@@ -42,33 +45,70 @@ class MeshedResourceFolderNode implements k8s.ClusterExplorerV1.Node {
         }
 
         const meshedResourceStdout = meshedResources.stdout;
-        const meshedResourceArray = meshedResourceStdout.split('\n');
+        const meshedResourceArray = meshedResourceStdout.split('\n').filter(
+            (value) => { return value.length > 0; }
+        );
 
-        return meshedResourceArray.map((meshedResource) => new MeshedResourceNode(meshedResource));
+        const namespaceToResource: NamespaceToResource = {};
+        const namespaces:Array<string> = [];
+
+        meshedResourceArray.forEach((value) => {
+            const namespaceAndPod = value.split('/');
+            const namespace = namespaceAndPod[0];
+            const podName = namespaceAndPod[1];
+
+            if (!namespaces.includes(namespace)) {
+                namespaces.push(namespace);
+                namespaceToResource[namespace] = [];
+            }
+
+            namespaceToResource[namespace].push(podName);
+        });
+
+        return namespaces.map(
+            (meshedNamespace: string) => new MeshedResourceNamespace(meshedNamespace, namespaceToResource[meshedNamespace])
+        );
     }
 
-    getTreeItem(): vscode.TreeItem {
+    getTreeItem (): vscode.TreeItem {
         const treeItem = new vscode.TreeItem('Linkerd Mesh', vscode.TreeItemCollapsibleState.Collapsed);
         treeItem.contextValue = 'linkerd.folder';
         return treeItem;
     }
 }
 
+class MeshedResourceNamespace implements k8s.ClusterExplorerV1.Node {
+    constructor(private readonly meshedNamespace: string, private readonly namespaceResources: string[]) {}
 
-class MeshedResourceNode implements k8s.ClusterExplorerV1.Node{
-    constructor(private readonly meshedResource: string) {}
-
-    async getChildren(): Promise<k8s.ClusterExplorerV1.Node[]> {
-        return [];
+    async getChildren (): Promise<k8s.ClusterExplorerV1.Node[]> {
+        return this.namespaceResources.map((podName: string) => new MeshedResourcePod(this.meshedNamespace, podName));
     }
 
-    getTreeItem(): vscode.TreeItem {
+    getTreeItem (): vscode.TreeItem {
         const treeItem = new vscode.TreeItem(
-            this.meshedResource,
+            this.meshedNamespace,
+            vscode.TreeItemCollapsibleState.Collapsed
+        );
+
+        treeItem.contextValue = 'linkerd.meshedNamespace';
+        return treeItem;
+    }
+}
+
+class MeshedResourcePod implements k8s.ClusterExplorerV1.Node {
+    constructor(private readonly meshedNamespace: string, private readonly meshedPodName: string) {}
+
+    getTreeItem (): vscode.TreeItem {
+        const treeItem = new vscode.TreeItem(
+            this.meshedPodName,
             vscode.TreeItemCollapsibleState.None
         );
 
-        treeItem.contextValue = 'linkerd.meshedResource';
+        treeItem.contextValue = 'linkerd.meshedPod';
         return treeItem;
+    }
+
+    async getChildren (): Promise<k8s.ClusterExplorerV1.Node[]> {
+        return [];
     }
 }
